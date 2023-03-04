1ec5b44efa6f17b890d9500d4289bf9d48b656ab
Marvelous Akpotu <still4marvelous@gmail.com>
2023-03-04 19:13:05 +0100

    Initial commit

---
+++ src/app/api/rephrase/route.ts
@@ -0,0 +1,31 @@
+export async function GET(request: Request) {
+  const { Configuration, OpenAIApi } = require("openai");
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+  });
+  const openai = new OpenAIApi(configuration);
+  const original_text = 'Benjamin Franklin FRS FRSA FRSE was an American polymath who was active as a writer, scientist, inventor, statesman, diplomat, printer, publisher, forger and political philosopher.';
+  const rephrase = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Rephrase this text: " + original_text,
+    temperature: 0.3,
+    max_tokens: 100,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return new Response(
+    JSON.stringify({
+      original_text: original_text,
+      rephrased_text: rephrase.data.choices,
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
+  )
+}
